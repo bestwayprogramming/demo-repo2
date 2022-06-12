f9bcd139039b0e5dee63818cb8b002b087fb25b1
Vivek Patel <vivekpatel@Viveks-MacBook-Pro.local>
2022-06-12 15:05:32 +0530

    sample
    dmfkvndfd

--- Main.java
+++ Main.java
@@ -1,17 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
-import java.io.*;
+import java.io.EOFException;
+import java.io.File;
 
-public class Main extends Thread {
+public class Main {
+
   public static void main(String[] args) {
     try {
-      File importFile=new File("reading.txt");
-      try (Scanner sc = new Scanner(importFile)) {
-        while(sc.hasNextLine()){
-            String str_2=sc.nextLine();
-            System.out.println(str_2);
-        }
+      File samplFile = new File("sample.txt");
+      Scanner sc = new Scanner(samplFile);
+      while(sc.hasNext()) {
+        String data = sc.next();
+        System.out.println(data);
       }
+      sc.close();
     } catch (Exception e) {
+      System.out.println("An error ocurred");
+      e.printStackTrace();
     }
   }
 }
